dd02d8f040cb9a666ecb64f3e45d5caa38d708b7
Ümmügülsüm Erol <92275196+ummugulsumm@users.noreply.github.com>
2023-08-11 17:47:14 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    // screens: {
+    //   'sc': '870px',
+    // },
+
+    container: {
+      center: true,
+      screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+      },
+    },
+
+
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif']
+      },
+
+      colors: {
+        'gega-red': '#BC1A45',
+        'gega-melon': '#FFD369',
+        'gega-grey': '#DDDDDD',
+        'gega-white': '#F7F7F7',
+      },
+
+      spacing: {
+        '128' : '32rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
