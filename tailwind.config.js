6e9b02927b7fabf7ec91c7700f282f94e1d03221
Ümmügülsüm Erol <92275196+ummugulsumm@users.noreply.github.com>
2023-08-17 15:45:58 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,22 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
 
-    // screens: {
-    //   'sc': '870px',
-    // },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1240px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
 
     container: {
       center: true,
